25ff643db36f3964ae3695ae6038cc15b673f9e9
Maron-X <120732521+Maron-X@users.noreply.github.com>
2024-04-25 13:05:52 +0200

    Update page title in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'tailwind-nuxt-2',
+    title: 'Rick & Morty',
     htmlAttrs: {
       lang: 'en',
     },
