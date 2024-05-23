a52591686daf3fa1151dfa75c1a0074a7bd958de
Maron-X <120732521+Maron-X@users.noreply.github.com>
2024-05-23 13:14:44 +0300

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  target: 'static',
+  // target: 'static',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
